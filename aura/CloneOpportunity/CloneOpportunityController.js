319a2de68d91e71e3f16a4317eeff6389bd8f563
Naval <naval.sfdc@gmail.com>
2019-02-20 21:23:39 +0530

    Version 1.4
    Allow users to update opportunity right after cloning the opportunity.
    
    Add/remove columns in product layout.

--- aura/CloneOpportunity/CloneOpportunityController.js
+++ aura/CloneOpportunity/CloneOpportunityController.js
@@ -1,86 +1,53 @@
 ({
-    doInit: function( cmp, event, helper ) {
-    	
-        // Get the list of the products from the source opportunity
-        let action = cmp.get("c.getOpportunityLineItems");
-        action.setParams({
-            opportunityId: cmp.get("v.recordId")	
-        });
         
-        action.setCallback( this, function( response ) { 
-        	let state = response.getState();
-            console.log(state);
-			if (state === "SUCCESS") {
-                console.log( response.getReturnValue() );
-				cmp.set( "v.lstLineItems", response.getReturnValue() );
-            }
-            else if (state === "INCOMPLETE") {
-            	
-            }
-            else if (state === "ERROR") {
-                var errors = response.getError();
-                if (errors) {
-                    if (errors[0] && errors[0].message) {
-                        console.log("Error message: " + 
-                                 errors[0].message);
-                    }
-                } else {
-                    console.log("Unknown error");
-                }
-            }
-        } );
-        
-        $A.enqueueAction(action);
+    validateFieldsAndhideCustomDockFooter: function(component, event, helper) {
+    	component.set("v.isDataChanged", true);	  
+        console.log(event.getParam('draftValues'));
+        helper.validate(component, event);
     },
     
-    // Function which creates a new opportunity 
-    // from the details of source/original opportunity
-    // Also clone the selected products 
-	doSaveAction : function(cmp, event, helper) {
-		
-        // Getting the cloned product fields to validate
-        let quantityFields = cmp.find("inputQuantity");
-        let unitPriceFields = cmp.find("inputUnitPrice");
+    cloneOpportunity: function(component, event, helper) {
+        // stop the form from submitting since we are going to clone the opportunity 
+        // so it will be done in the server side action
+        event.preventDefault();       
+        var fields = event.getParam("fields");
+        //fields["Id"] = component.get("v.recordId");
+        console.log(JSON.stringify(fields))
+       	
+        helper.cloneOppAndGetLineItems(component, fields);
         
-        var requiredFields = [];
-        if( quantityFields.length != undefined )
-        	requiredFields = requiredFields.concat( quantityFields );  
-        
-        if( unitPriceFields.length != undefined )
-        	requiredFields = requiredFields.concat( unitPriceFields );  
-        // Initialize the counter to zero - used to check validity of fields
-        var blank=0;
-        // If there are more than 1 fields
-        if( requiredFields.length != undefined ) {
-            console.log( quantityFields );
-            // Iterating all the fields
-            var allValid = requiredFields.reduce(function ( validSoFar, inputCmp ) {
-                console.log( validSoFar, inputCmp );
-                // return whether all fields are valid or not
-                return validSoFar && helper.checkForRequired( inputCmp, "Complete this field" );
-            }, true );
-            // If all fields are not valid increment the counter
-            if ( !allValid ) {
-                blank++;
-            }
-        } else {
-            // If there is only one field, get that field and check for validity (true/false)
-            var allValid = requiredFields;
-            // If field is not valid, increment the counter
-            if ( !helper.checkForRequired( allValid ) ) {
-                blank++;
-            }
-        }
-        // Call the helper method only when counter is 0
-        if(blank == 0) {
-            
-            helper.cloneOpportunityWithProducts( cmp, helper );
-        }	
-        
-	},
+    },
     
-    doCancelAction : function(cmp, event, helper) {
-    	$A.get("e.force:closeQuickAction").fire();    
-        cmp.destroy();
-    }
+    handleCloneLineItems: function(component, event, helper){
+        helper.cloneOppLineItems(component, event);  
+    },
+    
+    handleColumnSorting : function(component, event, helper) {
+        // assign the latest attribute with the sorted column fieldName and sorted direction
+    	component.set("v.sortedBy", event.getParam("fieldName"));
+    	component.set("v.sortedDirection", event.getParam("sortDirection")); 
+        helper.sortData(component, event.getParam("fieldName"), event.getParam("sortDirection"));
+    },
+    
+    /**
+     * Handling opp line item row action i.e. deleting a row
+     * */
+    handleRowAction: function (component, event, helper) {
+        var action = event.getParam('action');
+        var row = event.getParam('row');
+        console.log(action, row);
+        switch (action.name) {
+            case 'delete':
+                helper.deleteRow(component, row);
+                break;
+        }
+    },
+    
+    handleCancel : function(component, event, helper) {
+        $A.get("e.force:closeQuickAction").fire();
+    },
+    
+    navigateToOpp : function( component ) {
+        helper.navigateToRecord(component.get("v.clonedOppRecordId"));
+    },
 })
--- aura/CloneOpportunity/CloneOpportunityHelper.js
+++ aura/CloneOpportunity/CloneOpportunityHelper.js
@@ -1,5 +1,65 @@
 ({
-    navigateToRecord : function( recordId ) {
+    
+    cloneOppAndGetLineItems: function (component, fields){
+        
+    	let action = component.get("c.cloneOpportunityAndReturnProducts");
+        action.setParams({
+            opp: fields,
+            sourceOppRecordId : component.get("v.recordId")
+        });
+        
+        this.showSpinner(component);
+        
+        action.setCallback( this, function( response ) {
+            let state = response.getState();
+            if (state === "SUCCESS") {
+                component.set("v.saved", true);
+                console.log(response.getReturnValue());
+                let resObj = JSON.parse(response.getReturnValue());
+                
+                //Set the cloned opportunity record id
+                component.set("v.clonedOppRecordId", resObj.clonedOppId);
+                
+                // Set the data-table columns with additional action column 
+                // which will allow users to delete a line item 
+                var actions = [
+                    { label: 'Delete', name: 'delete' }
+                ];
+                let actionCol = { type: 'action', typeAttributes: { rowActions: actions, menuAlignment: 'left' } };
+                resObj.columns.push(actionCol);
+                
+                //Find the Product2.Name column and change the fieldName so that we can reference the parent name
+                let product2Col = resObj.columns.find(c=>c.fieldName == "Product2.Name");
+                if(product2Col.fieldName != undefined){
+                    product2Col.fieldName = "Product2Name";    
+                }
+				component.set('v.columns', resObj.columns);
+                
+                //Set the products data to display in the data-table
+                //In order to display product names, we will have to flatten the column names 
+                for (var i = 0; i < resObj.data.length; i++) {
+                    var row = resObj.data[i];
+                    if (row.Product2) row.Product2Name = row.Product2.Name;
+                }
+                
+                component.set('v.products', resObj.data);
+                
+            }
+            else if (state === "INCOMPLETE") {
+                console.error('ERROR');
+                this.handleErrors(component, [{message: 'Unable to clone the opportunity due to network error'}]);
+            }
+            else if (state === "ERROR") {
+                console.error(response.getError());
+                this.handleErrors(component, response.getError(), 'info');
+            }
+            this.hideSpinner(component);
+        } );
+
+        $A.enqueueAction(action);    
+    },
+    
+    navigateToRecord: function ( recordId ) {
         var navEvt = $A.get("e.force:navigateToSObject");
         navEvt.setParams({
             "recordId": recordId
@@ -7,55 +67,157 @@
         navEvt.fire();	
     },
     
-    // Function for checking the required fields
-    checkForRequired: function( inputCmp, message ) {
-        let value = inputCmp.get("v.value");
-        console.log( value );
-        if( value == null || value == '' ) {
-            inputCmp.set("v.errors", [{ message: message }]);
-            return false;
+    
+    cloneOppLineItems: function (component, event){
+    	let action = component.get("c.cloneOpportunityLineItems");
+        //console.log(component.get("v.recordId"));
+        //console.log(component.get("v.clonedOppRecordId"));
+        //console.log(component.get("v.products"));
+        //console.log(component.get("v.savedDraftValues"));
+        
+        if(typeof event.getParam === "function"){
+            component.set('v.savedDraftValues', event.getParam('draftValues'));
         }
         else {
-            //clear error
-            inputCmp.set("v.errors", null);
-            return true;
+        	component.set('v.savedDraftValues', []);    
+        }
+        
+        let isValidData = this.validate(component, event);
+        console.log(isValidData);
+        
+        if(isValidData){
+            let lstSourceLineItemIds = [];
+            for(let oli of component.get("v.products")){
+                lstSourceLineItemIds.push(oli.Id);	    
+            }
+            action.setParams({
+                sourceOppRecordId: component.get("v.recordId"),
+                clonedOppRecordId: component.get("v.clonedOppRecordId"),
+                lstSourceLineItemIds: lstSourceLineItemIds,
+                lstLineItemsChanges: component.get("v.savedDraftValues")
+            });
+    		
+            this.showSpinner(component);
+            
+            action.setCallback( this, function( response ) {
+                
+                let state = response.getState();
+                if (state === "SUCCESS") {
+                    this.navigateToRecord(response.getReturnValue());
+                }
+                else if (state === "INCOMPLETE") {
+                    this.handleErrors(component, [{message: 'Unable to clone the opportunity line items due to network problem'}]);
+                }
+                else if (state === "ERROR") {
+                    this.handleErrors(component, response.getError());
+                }
+                this.hideSpinner(component);
+            } );
+    
+            $A.enqueueAction(action);    
         }
     },
     
-    // Function which calls server method to create the opportunity
-    // and associated products
-    cloneOpportunityWithProducts: function( cmp, helper ) {
+    deleteRow: function (component, row){
+    	var rows = component.get('v.products');
+        var rowIndex = rows.indexOf(row);
+        rows.splice(rowIndex, 1);
+        component.set('v.products', rows);    
+    },
+    
+    sortData: function (component, fieldName, sortDirection) {
+        var products = component.get("v.products");
+        var reverse = sortDirection !== 'asc';
+        //sorts the rows based on the column header that's clicked
+        products.sort(this.sortBy(fieldName, reverse))
+        component.set("v.products", products);
+    },
+    
+    sortBy: function (field, reverse, primer) {
+        var key = primer ?
+            function(x) {return primer(x[field])} :
+            function(x) {return x[field]};
+        //checks if the two rows should switch places
+        reverse = !reverse ? 1 : -1;
+        return function (a, b) {
+            return a = key(a), b = key(b), reverse * ((a > b) - (b > a));
+        }
+    },
+    
+    handleErrors: function (component, errors, type, closeCallback){
         
-        // this apex method create a new opportunity and it's related opportunity products
-        let action = cmp.get("c.cloneOpportunityWithProducts");
-        action.setParams({
-            opportunityId: cmp.get("v.recordId"),
-            lstLineItems: cmp.get("v.lstLineItems")
+        let msgText = 'Unknown error';
+		let variant = 'error';
+        
+        if(type){
+            variant = type;
+        }
+
+        if (errors && Array.isArray(errors) && errors.length > 0) {
+            msgText = errors[0].message;
+        }
+        
+        console.error(msgText);
+        
+        component.find('notifLib').showNotice({
+            "variant": variant,
+            "header": variant == "error" ? "Something has gone wrong!" : "Information",
+            "message": msgText,
+            closeCallback: closeCallback ? closeCallback : null 
         });
+    },
+    
+    validate: function(component, event) {
+		
+        console.log('validate called');
+        let rows = {};
+        let isValid = true;
+        let items = component.get("v.savedDraftValues");
+        if(items == undefined){
+            items = event.getParam('draftValues');
+        }
+        for(let oli of items){
+            let messages = [];
+            let fieldNames = [];
+            for(let col of component.get("v.columns")){
+                if(col.required && oli[col.fieldName] == ''){
+                	messages.push('Enter a valid ' + col.label);	
+                    fieldNames.push(col.fieldName);
+                }    
+            }
+            if(fieldNames.length > 0){
+                isValid = false;
+            	let title = "We found " + fieldNames.length + " errors.";  
+                rows[oli.Id] = {
+                    title: title,
+                    messages: messages,
+                    fieldNames: fieldNames
+                };
+            } 
+        }
         
-        action.setCallback( this, function( response ) {
-            
-            let state = response.getState();
-            if (state === "SUCCESS") {
-                helper.navigateToRecord( response.getReturnValue() );
-            }
-            else if (state === "INCOMPLETE") {
-                alert(' ERROR : Unable to create the opportunity.');    
-            }
-            else if (state === "ERROR") {
-                var errors = response.getError();
-                if (errors) {
-                    if (errors[0] && errors[0].message) {
-                        console.log("Error message: " + 
-                                    errors[0].message);
-                    }
-                } else {
-                    console.log("Unknown error");
+        if(!isValid){
+            component.set('v.errors', {
+                rows: rows,
+                table: {
+                    title: 'Your entry cannot be saved. Fix the errors and try again.'
                 }
-            }
-            
-        } );
+             });
+        }
         
-        $A.enqueueAction(action);
-    }
+        return isValid;
+    },
+    
+    showSpinner : function( component ) {
+
+        $A.util.removeClass( component.find( 'spinner' ), 'slds-hide' );
+
+    },
+
+    hideSpinner : function( component ) {
+
+        $A.util.addClass( component.find( 'spinner' ), 'slds-hide' );
+
+    },
+
 })
